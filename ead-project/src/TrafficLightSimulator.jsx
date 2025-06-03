c133b3b08d24c13a04979655dd14dad8b051089c
KaranTejwani <karantejwani48@gmail.com>
2025-06-03 00:27:47 -0700

    Style: enhance UI with transitions and active styles

--- ead-project/src/TrafficLightSimulator.jsx
+++ ead-project/src/TrafficLightSimulator.jsx
@@ -20,12 +20,12 @@ function TrafficLightSimulator() {
 
     <div>
       <h3>Traffic Light Simulator</h3>
-      <div style={{width: "80px", height: "240px", backgroundColor: "black", borderRadius: "5%", padding: "40px"}}>
-        <div className="red" style={{ width: "70px", height: "70px", backgroundColor: myColor === "red" ? "red" : "", borderRadius: "50%" }}></div>
+      <div style={{width: "70px", height: "240px", backgroundColor: "black", borderRadius: "5%", padding: "40px", boxShadow: "0 0 20px rgba(0,0,0,0.5)"}}>
+        <div className="red" style={{ width: "70px", height: "70px", backgroundColor: myColor === "red" ? "red" : "grey", borderRadius: "50%", boxShadow: myColor === "red" ? "0 0 20px red" : "" }}></div>
         <br />
-        <div className="yellow" style={{ width: "70px", height: "70px", backgroundColor: myColor === "yellow" ? "yellow" : "", borderRadius: "50%" }}></div>
+        <div className="yellow" style={{ width: "70px", height: "70px", backgroundColor: myColor === "yellow" ? "yellow" : "grey", borderRadius: "50%", boxShadow: myColor === "yellow" ? "0 0 20px yellow" : "" }}></div>
         <br />
-        <div className="green" style={{ width: "70px", height: "70px", backgroundColor: myColor === "green" ? "green" : "", borderRadius: "50%" }}></div>
+        <div className="green" style={{ width: "70px", height: "70px", backgroundColor: myColor === "green" ? "green" : "grey", borderRadius: "50%", boxShadow: myColor === "green" ? "0 0 20px green" : ""  }}></div>
       </div>
     </div>
   );
