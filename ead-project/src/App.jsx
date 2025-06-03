6fe6f0f85c65f60515b81bc17354875977e5ba28
KaranTejwani <karantejwani48@gmail.com>
2025-06-03 00:06:58 -0700

    Refactor: convert to functional React component

--- ead-project/src/App.jsx
+++ ead-project/src/App.jsx
@@ -2,11 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Heading from './Heading'
 import TrafficLightSimulator from './TrafficLightSimulator'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- ead-project/src/TrafficLightSimulator.jsx
+++ ead-project/src/TrafficLightSimulator.jsx
@@ -1,16 +1,23 @@
+import { useState, useEffect } from "react";
 
 
 function TrafficLightSimulator() {
+
+  const [myColor, setColor] = useState("red")
+
+  useEffect(() => {
+
+  })
   return (
 
     <div>
       <h3>Traffic Light Simulator</h3>
       <div style={{width: "80px", height: "240px", backgroundColor: "black", borderRadius: "5%", padding: "40px"}}>
-        <div style={{ width: "70px", height: "70px", backgroundColor: "Red", borderRadius: "50%" }}></div>
+        <div className="red" style={{ width: "70px", height: "70px", backgroundColor: myColor === "red" ? "red" : "", borderRadius: "50%" }}></div>
         <br />
-        <div style={{ width: "70px", height: "70px", backgroundColor: "yellow", borderRadius: "50%" }}></div>
+        <div className="yellow" style={{ width: "70px", height: "70px", backgroundColor: myColor === "yellow" ? "yellow" : "", borderRadius: "50%" }}></div>
         <br />
-        <div style={{ width: "70px", height: "70px", backgroundColor: "green", borderRadius: "50%" }}></div>
+        <div className="green" style={{ width: "70px", height: "70px", backgroundColor: myColor === "green" ? "green" : "", borderRadius: "50%" }}></div>
       </div>
     </div>
   );
